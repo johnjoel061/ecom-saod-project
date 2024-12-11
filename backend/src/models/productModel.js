e675c0ff282bca6b16fd3649896e517281c75e76
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-11 23:00:51 +0800

    ...

--- backend/src/models/productModel.js
+++ backend/src/models/productModel.js
@@ -1,39 +1,70 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema({
+const productVariationSchema = new mongoose.Schema({
+  color: {
+    type: String,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    min: 0,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+const productSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: String,
     vendor: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Vendor",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Vendor",
+      required: true,
     },
     category: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
     },
     subcategory: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Category",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
     },
     brand: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "Brand",
-        required: true,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true,
     },
     image: [String],
     variations: [productVariationSchema],
     ratingAverage: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
     ratingQuantity: {
-        type: Number,
-        default: 0,
+      type: Number,
+      default: 0,
     },
-});
+    reviews: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+export const Product = mongoose.model("Product", productSchema);
