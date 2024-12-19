4008ba17c4e77ad5e0298a98c78d2344ff975950
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-19 19:36:00 +0800

    ...

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,35 @@
 import express from "express";
 import dotenv from "dotenv";
-import mongoose from "mongoose";
 import { dbConnect } from "./src/utils/utils.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import { errorHandler, notFoundErrorHandler } from "./src/middlewares/errorHandler.js";
 
+
+//Load Environment Variables from .env file
 dotenv.config();
+
+//connection to MongoDB
 dbConnect();
+
+// Initialize Express app
 const app = express();
 
-const PORT = process.env.PORT || 8000;
+//Middleware Setup
+app.use(helmet());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(cors());
 
+// API Routes
+
+// Error Handler Middlewares
+app.use(errorHandler);
+app.use(notFoundErrorHandler);
+
+//Starting the Server
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => {
     console.log(`Server is running at https://localhost:${PORT}`);
 });
