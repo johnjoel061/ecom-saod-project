07de3b4485f6025346168adcc1c8b6c05d03edad
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-19 18:59:04 +0800

    ...

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { dbConnect } from "./src/utils/utils";
 
 dotenv.config();
-
+dbConnect();
 const app = express();
 
 const PORT = process.env.PORT || 8000;
--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -1,5 +1,13 @@
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 export const generateToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET, { expiresIn: "30d" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
+
+export const dbConnect = mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("Database Connected"))
+  .catch((error) => {
+    console.error("MongoDB Connection Error: ", error);
+  });
