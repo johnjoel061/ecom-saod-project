ee6dbb86049d0ada257582ddb6eee24858d83eaf
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-19 19:05:24 +0800

    ...

--- backend/src/utils/utils.js
+++ backend/src/utils/utils.js
@@ -5,7 +5,7 @@ export const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
 };
 
-export const dbConnect = mongoose
+export const dbConnect = () => mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => console.log("Database Connected"))
   .catch((error) => {
