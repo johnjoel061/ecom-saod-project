e4a763e0ecb71fa22b7653221f1fab23fa969aed
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-23 17:21:33 +0800

    ...

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,5 +1,4 @@
-import { User }  from "../models/userModel.js";
-import bcrypt from "bcrypt";
+import { User } from "../models/userModel.js";
 import expressAsyncHandler from "express-async-handler";
 
 // @dest Register a new user
@@ -7,34 +6,36 @@ import expressAsyncHandler from "express-async-handler";
 // @access Public
 
 export const registerUser = expressAsyncHandler(async (req, res) => {
-    const {name, email, password} = req.body;
+  const { name, email, password, phone } = req.body;
+  console.log(req.body);
+  try {
+    // Find if the user already exists
+    const userExists = await User.findOne({ email });
 
-    try {
-        // Find if the user already exists
-        const userExists = await User.findOne({email});
-
-        if (userExists) {
-            return res.status(400).json({message: "User Already Exists"});
-        }
-
-        const user = await User.create({
-            name,
-            email,
-            password,
-        });
-
-        if(user) {
-            res.status(201).json({
-                _id: user._id,
-                name: user.name,
-                email: user.email,
-                role: user.role,
-            });
-        }else{
-            res.status(400).json({message: "Invalid User Data"});
-        }
-
-    } catch (error) {
-        
+    if (userExists) {
+      return res.status(400).json({ message: "User Already Exists" });
     }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      phone
+    });
+
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      });
+    } else {
+      res.status(400).json({ message: "Invalid User Data" });
+    }
+  } catch (error) {
+    // Handle errors
+    console.error("Error in registerUser:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
 });
