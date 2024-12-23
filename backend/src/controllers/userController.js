314d65f972b3e1172b5dc47fd63222fef8f2db28
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-23 21:18:28 +0800

    ...

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,10 +1,11 @@
 import { User } from "../models/userModel.js";
 import expressAsyncHandler from "express-async-handler";
 import { generateToken } from "../utils/utils.js"
-// @dest Register a new user
-// @router /api/users
-// @access Public
+import { AppError } from "../middlewares/errorHandler.js";
 
+// @dest Register a new user
+// @router /api/users/login  
+// @access Public
 export const registerUser = expressAsyncHandler(async (req, res) => {
   const { name, email, password, phone } = req.body;
 
@@ -40,6 +41,10 @@ export const registerUser = expressAsyncHandler(async (req, res) => {
   }
 });
 
+
+// @dest Login a user
+// @router /api/users/login
+// @access Public
 export const loginUser = expressAsyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({email});
@@ -56,5 +61,88 @@ export const loginUser = expressAsyncHandler(async (req, res) => {
     })
   } else{
     throw new Error("Invalid Email or Password!");
+  }
+});
+
+// @dest Get user profile
+// @router /api/users/profile
+// @access Private
+export const profile = expressAsyncHandler(async (req, res) => {
+  const { _id } = req.body;
+
+  // Find the user by ID
+  const user = await User.findOne(_id);
+  if (user){
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      phone: user.phone,
+      isActive: user.isActive
+    });
+  } else{
+    throw new Error("User Not Found! ");
+  }
+});
+
+// @dest Update user profile
+// @router /api/users/profile
+// @access Private
+export const updateProfile = expressAsyncHandler(async (req, res) => {
+  const { _id } = req.body;
+
+  // Find the user by ID
+  const user = await User.findOne(_id);
+  if (user){
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    user.address = req.body.address || user.address;
+    user.phone = req.body.phone || user.phone;
+
+    const updateUser = await User.save()
+     
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      phone: user.phone,
+      isActive: user.isActive,
+      address: user.address
+    });
+  } else{
+    throw new Error("User Not Found! ");
+  }
+});
+
+
+// @dest Get All user profile
+// @router /api/users/profile
+// @access Private
+export const getAllProfile = expressAsyncHandler(async (req, res) => {
+  // Find the user by ID
+  const users = await User.find();
+  if (users){
+    res.json(users);
+  } else{
+    throw new Error("User Not Found! ");
+  }
+});
+
+
+// @dest Delete user profile
+// @router /api/users/profile
+// @access Private
+export const deleteUserProfile = expressAsyncHandler(async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: "User Removed!" });
+  } catch (error) {
+    throw new AppError("User Not Found!");
+    
   }
 });
--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { deleteUserProfile, getAllProfile, loginUser, profile, registerUser, updateProfile } from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
+userRouter.post("/login", loginUser);
+userRouter.get("/profile", profile);
+userRouter.put("/profile", updateProfile);
+userRouter.get("/users", getAllProfile);
+userRouter.get("/users/:id", deleteUserProfile);
 
 export default userRouter;
