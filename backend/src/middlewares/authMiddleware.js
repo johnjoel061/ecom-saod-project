ab85ff1dd6f371c31f6c3ba3f8f2e42382e814d4
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-25 14:57:40 +0800

    ...

---
+++ backend/src/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from "jsonwebtoken";
+import { User } from "../models/userModel.js"
+import { AppError } from "./errorHandler.js"
+
+export const protect = async (req, res, next) => {
+    let token;
+
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try{
+            token = req.headers.authorization.split("")[1];
+            const decoded = jwt.verify(token.process.env.JWT_SECRET);
+            req.user = await User.findById(decoded.id).select("-password");
+            next()
+        }
+        catch(error){
+            throw new AppError
+        }
+    }
+}
