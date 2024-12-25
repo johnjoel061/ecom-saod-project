f4277bdf76738875d03731584b543b6dfdfc66a0
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-25 15:04:17 +0800

    ...

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { deleteUserProfile, getAllProfile, loginUser, profile, registerUser, updateProfile } from "../controllers/userController.js";
+import { authorize, protect } from "../middlewares/authMiddleware.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
 userRouter.post("/login", loginUser);
-userRouter.get("/profile", profile);
-userRouter.put("/profile", updateProfile);
-userRouter.get("/users", getAllProfile);
-userRouter.get("/users/:id", deleteUserProfile);
+userRouter.get("/profile", protect, profile);
+userRouter.put("/profile", protect, updateProfile);
+userRouter.get("/users", protect, authorize("admin"), getAllProfile);
+userRouter.delete("/users/:id", protect, deleteUserProfile);
 
 export default userRouter;
