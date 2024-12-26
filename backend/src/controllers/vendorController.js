2e6c608ada2483b35d4d516fe8460118e1f61ae5
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-26 16:06:38 +0800

    ...

--- backend/src/controllers/vendorController.js
+++ backend/src/controllers/vendorController.js
@@ -3,39 +3,56 @@ import expressAsyncHandler from "express-async-handler";
 import { AppError } from "../middlewares/errorHandler.js";
 
 // @dest Create a new Vendor
-// @router /api/vendor/  
+// @router /api/vendor/
 // @access Private
 export const createVendor = expressAsyncHandler(async (req, res) => {
-   try {
+  try {
     const newVendor = await Vendor.create(req.body);
     res.status(201).json({ status: true, data: newVendor });
-   } catch (error) {
+  } catch (error) {
     throw new AppError(error, 400);
-   } 
+  }
 });
-
 
 // @dest Get Vendor
-// @router /api/vendors/  
+// @router /api/vendors/
 // @access Public
 export const getVendors = expressAsyncHandler(async (req, res) => {
-   try {
+  try {
     const vendors = await Vendor.find().populate("user");
     res.status(201).json({ status: true, data: vendors });
-   } catch (error) {
+  } catch (error) {
     throw new AppError(error, 400);
-   } 
+  }
 });
-
 
 // @dest Get Vendor By Slug
-// @router /api/vendors/  
+// @router /api/vendors/
 // @access Public
 export const getVendorBySlug = expressAsyncHandler(async (req, res) => {
-   try {
-    const vendors = await Vendor.findOne({slug: req.params.slug});
+  try {
+    const vendors = await Vendor.findOne({ slug: req.params.slug }).populate(
+      "user",
+      "-password"
+    );
     res.status(201).json({ status: true, data: vendors });
-   } catch (error) {
+  } catch (error) {
     throw new AppError(error, 400);
-   } 
+  }
 });
+
+
+// @dest Update Vendor 
+// @router /api/vendors/
+// @access Public
+export const updateVendor = expressAsyncHandler(async (req, res) => {
+   try {
+     const vendor = await Vendor.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+     });
+     res.status(201).json({ status: true, data: vendor });
+   } catch (error) {
+     throw new AppError(error, 400);
+   }
+ });
+ 
--- backend/src/routes/vendorRoutes.js
+++ backend/src/routes/vendorRoutes.js
@@ -1,11 +1,19 @@
 import express from "express";
 import { protect } from "../middlewares/authMiddleware.js";
-import { createVendor, getVendors } from "../controllers/vendorController.js";
+import { createVendor, getVendorBySlug, getVendors, updateVendor } from "../controllers/vendorController.js";
 
 const vendorRouter = express.Router();
 
+//Create vendors route
 vendorRouter.post("/", createVendor);
+
+//Get vendors route
 vendorRouter.get("/all", getVendors);
 
+//Get Vendor by slug Route
+vendorRouter.get("/:slug", getVendorBySlug);
+
+//Get Vendor by slug Route
+vendorRouter.put("", updateVendor);
 
 export default vendorRouter;    
