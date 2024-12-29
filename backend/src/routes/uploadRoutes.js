c73fd0dbef28df894f9d5b596e8ecb247565bdec
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-29 19:18:12 +0800

    ...

--- backend/src/routes/uploadRoutes.js
+++ backend/src/routes/uploadRoutes.js
@@ -1,11 +1,12 @@
 import express from "express";
 import multer from "multer";
-import { uploadFile } from "../controllers/uploadController.js";
+import { deleteFile, uploadFile } from "../controllers/uploadController.js";
 
 const uploadRouter = express.Router();
 
 const upload = multer({ dest: "uploads/" });
 
 uploadRouter.post("/upload-file", upload.single("file"), uploadFile);
+uploadRouter.delete("/delete-file/:fileName",  deleteFile);
 
 export default uploadRouter;
