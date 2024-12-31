005cf77f0dc96b72dd3b33e97707ab8a389e46ec
johnjoel061 <alfabetejohnjoel@gmail.com>
2024-12-31 13:03:34 +0800

    ...

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -5,3 +5,26 @@ import { AppError } from "../middlewares/errorHandler.js";
 // @dest Create a new Order
 // @router /api/order/
 // @access Private
+
+export const createOrder = expressAsyncHandler(async (req, res) => {
+  try {
+    const order = new Order(req.body);
+    await order.save();
+    res.status(201).json({ status: true, data: order });
+  } catch (error) {
+    throw new AppError(error);
+  }
+});
+
+// @dest get all Order
+// @router /api/order/
+// @access Private
+export const getAllOrder = expressAsyncHandler(async (req, res) => {
+    try {
+      const order = new Order(req.body);
+      await order.save();
+      res.status(201).json({ status: true, data: order });
+    } catch (error) {
+      throw new AppError(error);
+    }
+  });
